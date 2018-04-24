717daf7a669a668dde3d8331fa7e59eba587fc80
hust-MC <251887584@qq.com>
2018-04-24 13:14:53 +0800

    主要修改样式匹配广告

---
+++ app/src/main/java/com/mc/shiyinqiao/myapplication/AdApplication.java
@@ -0,0 +1,8 @@
+package com.mc.shiyinqiao.myapplication;
+
+/**
+ * Created by shiyinqiao on 2018/4/23.
+ */
+
+public class AdApplication {
+}
---
+++ app/src/main/java/com/mc/shiyinqiao/myapplication/tensorflow/AdContent.java
@@ -0,0 +1,12 @@
+package com.mc.shiyinqiao.myapplication.tensorflow;
+
+import java.util.HashMap;
+
+/**
+ * Created by shiyinqiao on 2018/4/23.
+ */
+
+public class AdContent  {
+
+
+}
